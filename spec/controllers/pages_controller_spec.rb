47ffcadd64f6f794e5912b9d207837585f2ff10a
Luke Williams <luke@red-root.com>
2011-04-12 01:18:23 +0100

    Done with Static Pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+	render_views # render all views as well
 
-  describe "GET 'home'" do
+  describe "GET 'home'" do # general description, doesnt mean much
     it "should be successful" do
-      get 'home'
+      get 'home' # business side
       response.should be_success
+    end
+	
+	it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -13,6 +20,26 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+	
+	it "should have the right title" do # test the content
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+  
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+	
+	it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
